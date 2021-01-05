5facd66178d83359b390aa87e0d2569d845489d3
lam132409 <lam132409329@gmail.com>
2021-01-05 17:18:21 +0700

    12121

---
+++ src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.css']
+})
+export class ColorPickerComponent implements OnInit {
+
+  background = '#00e067';
+  constructor() { }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+  }
+
+  // tslint:disable-next-line:typedef
+  onChange(value: string) {
+    this.background = value;
+  }
+
+}
